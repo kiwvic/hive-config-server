1d453b1f9eed76cd46775f5417618bcfea21ca36
kiwvic <torogao@protonmail.com>
2022-06-17 14:53:22 +0300

    add gunicorn

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 from . import views
 
 urlpatterns = [
     path('', views.index)
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
